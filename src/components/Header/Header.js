31b936165160239491945925dd8a0a116ef5df2b
yogi <raoyogesh78@yahoo.in>
2021-01-22 12:05:12 +0100

    added forum icon in header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Header.css';
 import PersonIcon from '@material-ui/icons/Person';
 import { IconButton } from '@material-ui/core';
+import ForumIcon from '@material-ui/icons/Forum';
 
 function Header() {
     return (
@@ -13,7 +14,10 @@ function Header() {
                 className="header_logo"
                 src="https://tinder.com/static/apple-touch-icon.png" 
                 alt=""
-            />            
+            />
+            <IconButton>
+                <ForumIcon fontSize="large" className="header_icon" />            
+            </IconButton>
         </div>
     )
 }
